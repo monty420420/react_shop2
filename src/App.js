9ecc27755a2d9d0fb5802ef4dff322426a7f8c9c
Muntaeyeon <monty420420@naver.com>
2023-04-11 23:58:18 +0900

    bootstrap-img

--- src/App.js
+++ src/App.js
@@ -10,27 +10,25 @@ function App() {
     <div className="App">
       <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">shoeshop</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#link">cart</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+
+    <div className='main-bg'></div>
+    <div className='container'>
+      <div className='row'>
+           <div className='col-md-4'>d</div>
+           <div className='col-md-4'>d</div>
+           <div className='col-md-4'>d</div>
+      </div>
+    </div>
     </div>
   );
 }
