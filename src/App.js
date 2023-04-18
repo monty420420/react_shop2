f9f33f2547bdc34f9dba8f24664dfe1679a81ee9
Muntaeyeon <monty420420@naver.com>
2023-04-19 00:01:08 +0900

    자료형

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import './App.css';
 import { useState } from 'react';
-import a from './data.js'; //import
+import data from './data.js'
 
 function App() {
 
-  let [shoes] = useState();
+  let [shoes] = useState(data);
+  // console.log(shoes[0].title);
 
   return (
     <div className="App">
@@ -32,16 +33,16 @@ function App() {
            <div className='col-md-4'>
             <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"></img>
             {/* <img src="/logo192.png" width="80%"></img> public폴더 이미지 불러오기 /이용*/}
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
            </div>
            <div className='col-md-4'><img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"></img>
-           <h4>상품명</h4>
-            <p>상품설명</p>
+           <h4>{shoes[1].title}</h4>
+            <p>상{shoes[1].price}</p>
             </div>
            <div className='col-md-4'><img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"></img>
-           <h4>상품명</h4>
-            <p>상품설명</p>
+           <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].price}</p>
             </div>
       </div>
     </div>
--- src/data.js
+++ src/data.js
@@ -1,4 +1,33 @@
-let a = 10;
-let b = 100;
+//array 자료형
+// let a = [ 'kim', 20 ]
+// console.log(a[0]);
 
-export default a; //export
+//object 자료형
+// let b = { name : 'kim' , age : 20 }
+// console.log(b.name);
+
+
+
+let data = [
+    {
+        id : 0,
+        title : "white and black",
+        content : "born in france",
+        price : 12000
+    },
+    {
+        id : 1,
+        title : "red knit",
+        content : "born in seoul",
+        price : 11000
+    },
+    {
+        id : 2,
+        title : "grey yordan",
+        content : "born in the states",
+        price : 14000
+    }
+
+]
+
+export default data;
