9a78375404821de4c420547f1bf07d9eb1318443
Muntaeyeon <monty420420@naver.com>
2023-05-07 13:16:38 +0900

    react-router1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import './App.css';
 import { useState } from 'react';
 import data from './data.js'
+import {Routes, Route, Link} from 'react-router-dom'
 
 function App() {
 
@@ -26,7 +27,13 @@ function App() {
       </Container>
     </Navbar>
 
-    <div className='main-bg'></div>
+    <Link to="/">홈</Link>
+    <div></div>
+    <Link to="/detail">상세페이지</Link>
+    <Routes>
+        <Route path="/" element={
+        <>
+        <div className='main-bg'></div>
     <div className='container'>
       <div className='row'>
           {/* <Card shoes={shoes[0]} i={1}></Card>
@@ -41,6 +48,26 @@ function App() {
           }
       </div>
     </div>
+    </>
+    }> 
+    </Route>
+        <Route path='/detail' element={
+          <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            </div>
+            <div className="col-md-6">
+              <h4 className="pt-5">상품명</h4>
+              <p>상품설명</p>
+              <p>120000원</p>
+              <button className="btn btn-danger">주문하기</button> 
+            </div>
+          </div>
+        </div> 
+        } />
+    </Routes>
+    
     </div>
   );
 }
