4ef5680faf843d53b60ce28d474cd1d645eb7779
Muntaeyeon <monty420420@naver.com>
2023-05-07 21:39:54 +0900

    react-rotuer2

--- src/App.js
+++ src/App.js
@@ -5,80 +5,105 @@ import Navbar from 'react-bootstrap/Navbar';
 import './App.css';
 import { useState } from 'react';
 import data from './data.js'
-import {Routes, Route, Link} from 'react-router-dom'
+import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
+import Detail from './detail.js';
 
 function App() {
 
   let [shoes] = useState(data);
   // console.log(shoes[0].title);
 
+  let navigate = useNavigate();
+
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">shoeshop</Navbar.Brand>
+        <Navbar.Brand onClick={()=>{navigate('/')}}>shoeshop</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">cart</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/detail')}}>Detail</Nav.Link>
+            {/* <Nav.Link onClick={()=>{navigate('-1')}}>뒤로가기</Nav.Link> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
 
-    <Link to="/">홈</Link>
-    <div></div>
-    <Link to="/detail">상세페이지</Link>
+    {/* <Link to="/">홈</Link>
+    <Link to="/detail">상세페이지</Link> */}
     <Routes>
-        <Route path="/" element={
+     <Route path="/" element={
         <>
         <div className='main-bg'></div>
-    <div className='container'>
-      <div className='row'>
+      <div className='container'>
+        <div className='row'>
           {/* <Card shoes={shoes[0]} i={1}></Card>
           <Card shoes={shoes[1]} i={2}></Card>
           <Card shoes={shoes[2]} i={3}></Card> */}
           {
           shoes.map((a,i)=>{
             return (
-              <Card shoes={shoes[i]} i={i+1}></Card>
+              <Card shoes={shoes[i]} i={i+1}></Card>  // 0부터 돌아서 i값에 +1을 해주면 1->2->3이렇게 된다
             )
           })
           }
+        </div>
       </div>
-    </div>
-    </>
-    }> 
-    </Route>
-        <Route path='/detail' element={
-          <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-            </div>
-            <div className="col-md-6">
-              <h4 className="pt-5">상품명</h4>
-              <p>상품설명</p>
-              <p>120000원</p>
-              <button className="btn btn-danger">주문하기</button> 
-            </div>
-          </div>
-        </div> 
-        } />
+        </>
+        }> 
+     </Route>
+        <Route path='/detail' element={<Detail/>} />
+
+        {/* nested Route */}
+        <Route path='/about' element={<About/>}>
+          <Route path='member' element={<div>about의 nestedRotuer</div>}></Route>  {/* /about/member */} {/*about페이지 안의 페이지*/}
+          <Route path='location' element={<div>about의 nestedRotuer2</div>}></Route> {/* /about/location */}
+        </Route>  
+
+        <Route path='/event' element={<Event/>}>
+          <Route path='one' element={<div>첫 주문시 양배추즙 서비스</div>}></Route>
+          <Route path='two' element={<div>생일기념 쿠폰받기</div>}></Route>
+        </Route>
+        
+        <Route path='*' element={404} /> {/*404페이지 */}
     </Routes>
     
     </div>
   );
 }
+
+//카드 컴포넌트
 function Card(props) {
   return (
     <div className='col-md-4'>
     <img src={"https://codingapple1.github.io/shop/shoes"+ props.i + '.jpg'} width="80%"></img> {/*문자형으로 바꿔서 이용 */}
+    {/* <img src={"https://codingapple1.github.io/shop/shoes"+ (props.i+1) + '.jpg'} width="80%"></img> */} {/*이렇게 props.i에 +1가능 */}
     {/* <img src="/logo192.png" width="80%"></img> public폴더 이미지 불러오기 /이용*/}
     <h4>{props.shoes.title}</h4>
     <p>{props.shoes.price}</p>
    </div>
+  )
+}
+
+//about 컴포넌트 //nested Router 연습용
+function About() {
+  return (
+    <div>
+      <h4>정보</h4>
+      <Outlet></Outlet> {/*nested Router를 보여주는 역할 */}
+    </div>
+  )
+}
+
+//event 컴포넌트
+function Event() {
+  return(
+    <div>
+      <h3>오늘의 이벤트</h3>
+      <Outlet></Outlet> 
+    </div>
   )
 }
 
