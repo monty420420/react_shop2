ca585936813d83b9c96bf1239d9dffdb07070ffe
Muntaeyeon <monty420420@naver.com>
2023-05-13 19:14:43 +0900

    ui animation

--- src/detail.js
+++ src/detail.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import data from './data.js'
+import Nav from 'react-bootstrap/Nav';
 
 function Detail(props) {
 
@@ -35,11 +36,15 @@ function Detail(props) {
       setInputValue(event.target.value);
     };
     
-
-
+    let [visibility, setVisibility] = useState(''); //빈칸의 스테이트 생성
+    useEffect(()=>{
+         setTimeout(()=>{         //1초뒤에 visible이라는 글자를 classname의 container뒤에 붙힐거임
+          setVisibility('visible') 
+         }, 1000)
+    },[visibility])
 
   return(
-    <div className="container">
+    <div className={'container ' + visibility}> {/*classname에 클래스이름을 넣으며 조절 */}
        
          {
           alert === true ? <div className="alert alert-warning">2초이내 구매시 할인</div> : null
@@ -55,10 +60,65 @@ function Detail(props) {
         <p>{product.content}</p>
         <p>{product.price}원</p>
         <button className="btn btn-danger">주문하기</button> 
-      </div>
+      </div>  
     </div>
+    <TabMenu />
+    
   </div> 
   )
+}
+
+
+function TabMenu(){
+
+   //tap메뉴
+   let [tap, setTap] = useState(0);  //onclick이벤트 이용해서 useState가 0, 1, 2 일때 각각 원하는 화면 보여줌
+
+   //tap메뉴 fade이벤트
+   let [fade, setFade] = useState('');
+  //  console.log(fade)
+   useEffect(()=>{
+    let timer2 = setTimeout(()=> {     //0.1초후에 fade에 end를 넣어줌
+      setFade('end');    
+    }, 100)
+    return()=>{
+      clearTimeout(timer2)
+      setFade('')  //위 setTimeout 동작전 실행 fade에 ''공백란 넣어줌
+    }    
+   }, [tap]);
+
+   return(
+    <>
+    <Nav
+      variant="tabs"
+      // activeKey="/home"
+      defaultActiveKey="link-1"
+    >
+      <Nav.Item>
+        <Nav.Link onClick={()=> setTap(0)} eventKey="link-1">버튼1</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link onClick={()=> setTap(1)} eventKey="link-2">버튼2</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link onClick={()=> setTap(2)} eventKey="link-3">버튼3</Nav.Link>
+      </Nav.Item>
+    </Nav>
+    <div className={'start ' +fade}>  
+  {
+    tap === 0 ? (<div>a</div>) : null
+  }
+      {
+    tap === 1 ? (<div>b</div>) : null
+  }
+      {
+    tap === 2 ? (<div>c</div>) : null
+  }
+  </div>
+    </>
+
+   )
+  
 }
 
 export default Detail;
