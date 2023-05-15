954027493c4c57a2e3c8c5fe0acedf9379c0f98c
Muntaeyeon <monty420420@naver.com>
2023-05-15 20:05:51 +0900

    redux 바인딩

--- src/Cart.js
+++ src/Cart.js
@@ -1,6 +1,23 @@
 import {Table} from 'react-bootstrap'
+import { useSelector } from 'react-redux'
 
 function Cart() {
+    
+    //1.
+    // let a = useSelector((state)=> { return state })
+
+    //2.
+    // let a = useSelector((state)=>  state.user )
+    // console.log(a.user);
+    // console.log(a);
+
+    //3.
+    // let b = useSelector((state)=> { return state.user })
+    // console.log(b);
+    
+    let state = useSelector((state)=> state)
+    // console.log(state.cart[0]);
+
     
     return(
         <div>
@@ -14,12 +31,22 @@ function Cart() {
             </tr>
         </thead>
         <tbody>
-            <tr>
+            {/* <tr>
                 <td>1</td>
-                <td>신발</td>
+                <td>{state.cart[0].name}</td>
                 <td>2</td>
                 <td>확인</td>
-            </tr>
+            </tr> */}
+            {
+                state.cart.map((a,i)=>
+                    <tr key={i}>
+                        <td>{state.cart[i].id}</td>
+                        <td>{state.cart[i].name}</td>
+                        <td>{state.cart[i].count}</td>
+                        <td>확인</td>
+                    </tr>
+                )
+            }
         </tbody>
      </Table>
         </div>
